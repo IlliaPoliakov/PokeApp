55bc5083fa997205cf2c6b5f66389d131625e7d6
Illia_Poliakov <illiapoliakov1@gmail.com>
2022-11-15 01:53:25 +0300

    implement mainTableView data handler

--- PokeApp/PokeApp/Domain/Model/Pokemon.swift
+++ PokeApp/PokeApp/Domain/Model/Pokemon.swift
@@ -7,18 +7,20 @@
 
 import Foundation
 
-struct Pokemon {
+struct Pokemon: Hashable {
   var name: String
   var imageUrl: URL
   var weight: Int32
   var height: Int32
   var types: [String]
+  var isLovely: Bool
   
-  init(name: String, imageUrl: URL, weight: Int32, height: Int32, types: [String]) {
+  init(name: String, imageUrl: URL, weight: Int32, height: Int32, types: [String], isLovel: Bool) {
     self.name = name
     self.imageUrl = imageUrl
     self.weight = weight
     self.height = height
     self.types = types
+    self.isLovely = isLovel
   }
 }
--- PokeApp/PokeApp/UI/ViewControllers/ViewControllersDataHandlers/MainTableView.swift
+++ PokeApp/PokeApp/UI/ViewControllers/ViewControllersDataHandlers/MainTableView.swift
@@ -6,4 +6,118 @@
 //
 
 import Foundation
+import UIKit
 
+enum Section {
+  case mainSection
+}
+
+enum SortState {
+  case alphabetical
+  case notAlphabetical
+  case lovely
+  case non
+}
+
+class MainTableView: NSObject, UITableViewDelegate {
+  
+  // -MARK: - Properties -
+  
+  var tableView: UITableView?
+  var pokemons: [Pokemon]?
+  var lovelyPokemons: [Pokemon]? {
+    get {
+      return pokemons?.filter { $0.isLovely == true }
+    }
+  }
+  var sortState: SortState = .non
+  
+  lazy var dataSource: UITableViewDiffableDataSource<Section, Pokemon> =  UITableViewDiffableDataSource<Section, Pokemon> (tableView: tableView!) {
+    tableView, indexPath, itemIdentifier in
+    
+    guard let cell = tableView.dequeueReusableCell(withIdentifier: "MainVCCustomCell")
+            as? MainTableViewCell
+    else {
+      fatalError("Can't deque custom cell in MainVC.")
+    }
+    
+    let pokemon: Pokemon
+    
+    switch self.sortState {
+    case .lovely:
+      pokemon = self.lovelyPokemons![indexPath.row]
+    default:
+      pokemon = self.pokemons![indexPath.row]
+    }
+    
+    cell.bind(withPokemon: pokemon)
+    
+    return cell
+  }
+  
+  
+  // -MARK: - Functions -
+  
+  func configureSnapshot(){
+    var snapshot = NSDiffableDataSourceSnapshot<Section, Pokemon>()
+    
+    snapshot.appendSections([.mainSection])
+    
+    guard pokemons != nil
+    else {
+      dataSource.apply(snapshot, animatingDifferences: true)
+      return
+    }
+    
+    switch sortState {
+    case .lovely:
+      snapshot.appendItems(lovelyPokemons!, toSection: .mainSection)
+    default:
+      snapshot.appendItems(pokemons!, toSection: .mainSection)
+    }
+   
+    dataSource.apply(snapshot, animatingDifferences: true)
+  }
+  
+  func updateSnapshot(withNewPokemons pokemons: [Pokemon]?) {
+    guard pokemons != nil
+    else {
+      return
+    }
+    
+    if self.pokemons != nil {
+      pokemons!.forEach { pokemon in
+        self.pokemons!.append(pokemon)
+      }
+    }
+    else {
+      self.pokemons = pokemons!
+    }
+    
+    self.updatePresentation(withSortState: self.sortState)
+  }
+  
+  func updatePresentation(withSortState sortState: SortState) {
+    guard pokemons != nil
+    else {
+      return
+    }
+    
+    switch sortState {
+    case .alphabetical:
+      self.pokemons = pokemons!.sorted {
+        $0.name < $1.name
+      }
+ 
+    case .notAlphabetical:
+      self.pokemons = pokemons!.sorted {
+        $0.name > $1.name
+      }
+    
+    default:
+      break
+    }
+    
+    configureSnapshot()
+  }
+}
