35cc340c920c8bde0887407b4c1917538f7373b1
Illia_Poliakov <illiapoliakov1@gmail.com>
2022-11-15 11:35:10 +0300

    add addPokemons method in repoImpl

--- PokeApp/PokeApp/Data/Repository/PokemonRepositoryImpl.swift
+++ PokeApp/PokeApp/Data/Repository/PokemonRepositoryImpl.swift
@@ -40,7 +40,75 @@ class PokemonRepositoryImpl: PokemonRepository {
     return modelPokemons
   }
   
-
+  func addPokemons(_ completion: @escaping ([Pokemon]?, String?) -> Void) {
+    var prevNextUrl =  localDataSource.getPrevNextUrl()
+    
+    if prevNextUrl == nil {
+      prevNextUrl = localDataSource.saveNewPrevNextUrl(withPreviousUrl: nil,
+                                                       withNextUrl: URL(string: "https://pokeapi.co/api/v2/pokemon"))
+    }
+    
+    guard prevNextUrl?.nextUrl != nil
+    else {
+      DispatchQueue.main.async {
+        completion(nil, "No Pokemons Yet")
+      }
+      return
+    }
+    
+    DispatchQueue.global(qos: .userInitiated).async {
+      
+      self.remoteDataSource.downloadData(withUrl: prevNextUrl!.nextUrl!) { data, error in
+        guard data != nil
+        else {
+          DispatchQueue.main.async {
+            completion(nil, error)
+          }
+          return
+        }
+        
+        let jsonPokemonGroup = self.parsePokemonGroup(withData: data!)
+        let modelPokemons: [Pokemon]? = Pokemon.convertFromJsonPokemons(
+          withJsonPokemons: jsonPokemonGroup!.results)
+        DispatchQueue.main.async {
+          completion(modelPokemons, nil)
+        }
+        
+        prevNextUrl?.nextUrl = jsonPokemonGroup?.next
+        prevNextUrl?.previousUrl = jsonPokemonGroup?.previous
+        
+        let group = DispatchGroup()
+        
+        var expendedModelPokemons = [Pokemon]()
+        
+        modelPokemons!.forEach { pokemon in
+          group.enter()
+          self.remoteDataSource.downloadData(withUrl: pokemon.descriptionUrl) {
+            descriptionData, descriptionError in
+            guard descriptionData != nil
+            else {
+              group.leave()
+              return //mistake can occure
+            }
+            
+            let descriptionJsonPokemon = self.parsePokemonDescription(withData: descriptionData!)
+            
+            expendedModelPokemons.append(Pokemon
+              .convertFromDescriptionJsonPokemon(withJsonPokemon: descriptionJsonPokemon!,
+                                                 withPokemon: pokemon))
+            
+            group.leave()
+          }
+        }
+        
+        group.wait()
+        
+        DispatchQueue.main.async {
+          completion(expendedModelPokemons, nil)
+        }
+      }
+    }
+  }
   
   func parsePokemonGroup(withData data: Data) -> JsonPokemonsGroup? {
     
